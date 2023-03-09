bd64f54b0f5f57e403af88bc41cd2b9f3b559b49
Phenry-Gaia <pierre-henry.dorglandes@gaia.fr>
2023-03-09 15:22:16 +0100

    premiers exercices de language

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 console.log('hello there')
+var myVar = 'hello there'; //declarer une variable -> prohibé et remplacer par let
+
+/*commentaire
+multi-ligne */
+
+const test = 'une constante'; //reaffectation impossible
+
+let age = 30; //reaffectation possible
+
+age = 'trente'; //changement de type
+
+let myString = 'text';
+
+let anotherString = `allow apostrophe ' and multiple line
+string`;
+
+let nom = 'Jean'; let prenom = 'Antony'; let nomComplet = nom + ' ' + prenom; //concat
+
+let myConcat = '';
+
+myConcat = myString.concat('test', 'yolo'); //fail, a verifier
+console.log(myConcat);
+
+let mytexte = 'tester la longueur';
+let myLongueur = myLongueur.lenght;
+
